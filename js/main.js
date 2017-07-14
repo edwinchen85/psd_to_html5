3692f3559b8762ed63e0fad97f42f6da65ff21b4
Edwin Chen <edwinchen85@gmail.com>
2017-07-14 16:01:38 +0800

    Add iphone animation scripts

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.js-wp-2').waypoint(function(direction) {
+    $('.js-wp-2').addClass('animated slideInUp');
+  }, {
+    offset: '70%'
+  });
+
+  $('.js-wp-3').waypoint(function(direction) {
+    $('.js-wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  });
+
+  $(".iphone-btn").delay(1500).animate( {bottom: "+=-3px"}, 300 );
+  $(".iphone-btn").delay(300).animate( {top: "+=-3px"}, 100 );
+});
